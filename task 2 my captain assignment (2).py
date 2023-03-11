0ea3ab4df4d75419994830f338607c30f14da59a
kavya-12803 <127583812+kavya-12803@users.noreply.github.com>
2023-03-11 12:19:42 +0530

    Add files via upload

---
+++ task 2 my captain assignment (2).py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+mylist1 = [ 12,-7,5,64,-14]
+for num in mylist1 :
+    if num>=0 :
+        print (num)
+
+
+# In[4]:
+
+
+mylist2=[12,14,-95,3]
+for num in mylist2 :
+    if num >=0 :
+        print ([num])
+
+
+# In[ ]:
+
+
+
+
